f7d020ae3e6c48ddecd157d9bee258eac430d9df
AllexisAlvarico <C00242855@itcarlow.ie>
2021-11-17 13:35:53 +0000

    changed the functions at HealthSystem.h

--- include/Entity.h
+++ include/Entity.h
@@ -8,11 +8,16 @@ class Entity
 {
 public:
     Entity();
-    void addComponent() {};
-    void removeComponent(){};
-    std::vector<Component> m_getComponent(){return m_component;};
+    void addComponent(Component* t_c) { m_component.push_back(t_c);};
+    void removeComponent(Component* t_c){
+        m_component.erase(std::remove_if(m_component.begin(), m_component.end(),
+        [&](const auto& component){
+            return component->name() == c->name();
+        }),m_component.end());
+    }
+    std::vector<Component*>& m_getComponent(){return m_component;};
     
 private:
     int m_id;
-    std::vector<Component> m_component;
+    std::vector<Component*> m_component;
 };
--- include/HealthSystem.h
+++ include/HealthSystem.h
@@ -1,13 +1,31 @@
 #pragma once
 #include <iostream>
 #include "Entity.h"
+#include "HealthComponent.h"
 
 class HealthSystem
 {
 private:
     std::vector<Entity> m_entities;
 public:
-    void addEntity(Entity t_e){}
-    void update(){std::cout << "HealthSystem Update" << std::endl;}
+    void addEntity(Entity t_e){m_entities.push_back(t_e);}
+    void update(){
+
+        for(Entity& t_e : m_entities)
+        {
+            for(Component* t_c : t_e.m_getComponent()){
+
+                HealthComponent* m_healthComp{dynamic_cast<HealthComponent*>(t_c)};
+
+                if(m_healthComp)
+                {
+                    std::cout << "Component: " << m_healthComp->m_id << "Health: " << m_healthComp->getHealth() << std::endl;
+                }
+
+            }
+        }
+        //std::cout << "HealthSystem Update" << std::endl;
+    }
 };
 
+   
